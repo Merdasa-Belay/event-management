e49be3770642fb7bf6ee28aa899bb812a0448363
Merdasa-Belay <merdasabelay@gmail.com>
2024-09-19 20:23:01 +0300

    CRUD operations

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -17,7 +17,9 @@ class EventController extends Controller
     }
 
     /**
-
+     * Store a newly created resource in storage.
+     */
+    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -52,16 +54,29 @@ class EventController extends Controller
     /**0
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        // Validate the incoming request
+        $validatedData = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'sometimes|date',
+            'end_time' => 'sometimes|date|after:start_time'
+        ]);
+
+
+        $event->update($validatedData);
+
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
         //
+        $event->delete();
+        return response(status: 204);
     }
 }
