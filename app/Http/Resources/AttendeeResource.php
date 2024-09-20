c8dfbc5ebd111d7e2574a906c16784cfd9935494
Merdasa-Belay <merdasabelay@gmail.com>
2024-09-20 15:00:18 +0300

    CRUD operations

--- app/Http/Resources/AttendeeResource.php
+++ app/Http/Resources/AttendeeResource.php
@@ -19,8 +19,7 @@ class AttendeeResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'created_at' => $this->created_at->toDateTimeString(),
-            'updated_at' => $this->updated_at->toDateTimeString(),
+            'description' => $this->description
         ];
     }
 }
